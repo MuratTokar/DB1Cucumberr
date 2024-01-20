07dc1e5b680ab01fa3adb90a1f3703c7bc68e4e9
murat <murattokar26@gmail.com>
2024-01-20 01:24:41 +0100

    19.01

--- src/main/java/configg/TestConfig.java
+++ src/main/java/configg/TestConfig.java
@@ -1,4 +1,21 @@
 package configg;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Data
 public class TestConfig {
+    // config file nin pojo sudur.Okunmasi kolay olsun diye file den buraya atariz
+    private Application application;
+    private List<User> users = new ArrayList<User>();
+    private Tests tests;
+    private Browser chrome;
+    private Browser edge;
+    private Browser firefox;
+    private Map<String ,String> variables;
+
+
 }
+
